efe79fd3651beb5175d0055359fe0ef839449096
waadAlobaidi <waadmuhammed911@gmail.com>
2024-03-10 16:42:39 +0300

    first commit

---
+++ src/test/java/junit/assignment01.java
@@ -0,0 +1,12 @@
+package junit;
+
+import java.sql.SQLOutput;
+
+public class assignment01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+
+}
